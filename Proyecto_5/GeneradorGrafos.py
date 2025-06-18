f71d9404f3c91b7e6d043e0889863f810c995ed5
cesar-amg <cmendezg2025@cic.ipn.mx>
2025-06-17 22:11:46 -0600

    Add files via upload

---
+++ Proyecto_5/GeneradorGrafos.py
@@ -0,0 +1,60 @@
+import Grafo as gr
+import Visualizar as vis
+'''
+grafo = gr.Grafo()
+grafo.ErdosRenyi(100, 300)
+vis.spring(grafo)
+'''
+'''
+grafo =gr.Grafo()
+grafo.ErdosRenyi(500,700)
+vis.spring(grafo)
+'''
+'''
+grafo =gr.Grafo()
+grafo.Gilbert(100,.5)
+vis.spring(grafo)
+'''
+'''
+grafo =gr.Grafo()
+grafo.Gilbert(500,.25)
+vis.spring(grafo)
+'''
+'''
+grafo =gr.Grafo()
+grafo.GeoSimple(100,15)
+vis.spring(grafo)
+'''
+'''
+grafo =gr.Grafo()
+grafo.GeoSimple(500,25)
+vis.spring(grafo)
+'''
+'''
+grafo =gr.Grafo()
+grafo.BarabasiAlbertInverso(100,10)
+vis.spring(grafo)
+'''
+'''
+grafo =gr.Grafo()
+grafo.BarabasiAlbertInverso(500,20)
+vis.spring(grafo)
+'''
+""""
+grafo =gr.Grafo()
+grafo.DorogovtsevMendes(100)
+vis.spring(grafo)"""
+
+'''
+grafo =gr.Grafo()
+grafo.DorogovtsevMendes(500)
+vis.spring(grafo)
+'''
+'''
+grafo =gr.Grafo()
+grafo.Malla(10,10)
+vis.spring(grafo)
+'''
+grafo =gr.Grafo()
+grafo.Malla(25,20)
+vis.spring(grafo)
